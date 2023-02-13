36980704518e06479098427e98d22b87eecf3076
Nazebul-rudhro <Nazebul2022@gmail.com>
2023-02-13 11:14:55 +0600

    first commit

---
+++ admin/product/inc/addButton.php
@@ -0,0 +1,21 @@
+
+
+<form action="" method="post" class="form">
+    <div>
+        <label for="book">Book Name</label>
+        <input type="text" name="bName" id="book" class="form-control" required>
+    </div>
+    <div>
+        <label for="author">Athor Name</label>
+        <input type="text" name="aName" id="author" required>
+    </div>
+    <div>
+        <label for="quantity">Product Quantity</label>
+        <input type="tel" name="pQuantity" id="quantity" required>
+    </div>
+    <div>
+        <label for="price">Price</label>
+        <input type="tel" name="pPrice" id="price" required>
+    </div>
+</form>
+
